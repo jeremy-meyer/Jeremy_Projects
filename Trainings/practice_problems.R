c2ecbf87ba878ed0517beca54408e57cdbe3745c
Jeremy Meyer <jeremymeyer23@hotmail.com>
2024-06-17 19:46:32 -0600

    R training changes

---
+++ Trainings/practice_problems.R
@@ -0,0 +1,32 @@
+# Example Problems
+
+# Create and store a vector of numbers from 1 to 100. Multiply each value by 5
+
+
+# Remove the 10th element from the above vector
+
+
+# Load in a dataset called mtcars (should be already stored). Print all rows with mpg<15. 
+# How many rows are there?
+
+
+# Write a for loop / conditional to print out all even numbers between 1-50
+
+
+# Use for loops to print all pairs of integers that multiply to get 60.
+
+
+# Write a coin flip function that returns "H" or "T" with 50 % probability
+
+
+# To check your function, plot a basic histogram of the total number of heads/tails
+# after running your function 1000 times.
+
+
+# Create a simulation to determine the approximate probability of flipping 
+# exactly 6 heads out of 20 flips. 
+
+
+# (Harder) Suppose that you flip 100 coins. Create a simulation and determine
+# an approximate probability of flipping 5+ heads in a row somewhere within the 100 flips? 
+# 8+ in a row?
