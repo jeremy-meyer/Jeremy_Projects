3192b987dd55c7e0c308f03f1d7e49f480d214fb
J-Money <jeremymeyer23@gmail.com>
2025-09-30 21:18:37 -0600

    Making charts prettier, making calplot interactive

--- weather/dashboards/temperature_dash.py
+++ weather/dashboards/temperature_dash.py
@@ -5,14 +5,17 @@ import plotly.express as px
 import plotly.io as pio
 import plotly.graph_objects as go
 from plotly_calplot import calplot
+import os
 
-pio.templates.default = "seaborn"
+pio.templates.default = "plotly_dark"
 pio.renderers.default = "browser"
+print(os.getcwd())
+# TODO: Figure out why assets folder isn't working properly with the dark dropdown css
 
-# Initialize the app - incorporate a Dash Bootstrap theme
-external_stylesheets = [dbc.themes.DARKLY]
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+# Initialize the app
+# external_stylesheets = [dbc.themes.DARKLY, 'dark_dropdown.css']
 
+app = Dash(__name__) #, external_stylesheets=external_stylesheets, assets_folder='./weather/dashboards/assets/')
 # INPUT DATA
 # Create 1/2 index for leap year so we can intepret "hottest day of year" correctly
 # This will put Feb 29th between 2-28 and 3-1 in the model
@@ -29,12 +32,15 @@ normals = pd.read_csv("weather/output_sources/temp_normals.csv")
 
 temps['day_of_year'] = temps['date'].apply(gen_DoY_index)
 temps['range'] = temps['max_temp'] - temps['min_temp']
+temps['year'] = temps['date'].dt.year
+temps_minmax = (temps['year'].min(),temps['year'].max())
 
 # add a column rank to temp that ranks how hot each day was relative to all other years
 temps['high_rank'] = temps.groupby('day_of_year')['max_temp'].rank(ascending=False, method='min')
 temps['low_rank'] = temps.groupby('day_of_year')['min_temp'].rank(ascending=True, method='min')
+temps_full = temps.merge(normals, on='day_of_year', how='left')
 
-recent = temps[temps['date'] >= '2020-01-01'].merge(normals, on='day_of_year', how='left')
+recent = temps_full[temps_full['date'] >= '2020-01-01']
 
 record_highs = recent[recent['high_rank'] == 1.0]
 record_lows = recent[recent['low_rank'] == 1.0]
@@ -75,7 +81,7 @@ fig.add_trace(
 fig.add_trace(
     go.Scatter(
         x=recent['date'], y=recent['max_temp_p90'], mode='lines', name='90th Percentile High',
-        line=dict(color='darkred', width=1, dash='dot')
+        line=dict(color='red', width=1, dash='dot')
     ),
 )
 fig.add_trace(
@@ -83,7 +89,7 @@ fig.add_trace(
         x=list(recent['date']) + list(recent['date'])[::-1],
         y=list(recent['normal_high']) + list(recent['normal_low'])[::-1],
         fill='toself',
-        fillcolor='rgba(100,100,100,0.2)', 
+        fillcolor='rgba(125,125,125,0.25)', 
         line_color='rgba(255,255,255,0)',
         showlegend=True,
         name='Normal',
@@ -114,7 +120,7 @@ fig.add_trace(
         x=low_max['date'],
         y=low_max['max_temp'],
         mode='markers',
-        marker=dict(color='royalblue', size=8, symbol='square'),
+        marker=dict(color='royalblue', size=6, symbol='square'),
         name='Coldest High'
     )
 )
@@ -123,13 +129,13 @@ fig.add_trace(
         x=high_min['date'],
         y=high_min['min_temp'],
         mode='markers',
-        marker=dict(color='mediumvioletred', size=8, symbol='square'),
+        marker=dict(color='mediumvioletred', size=6, symbol='square'),
         name='Warmest Low'
     )
 )
 
 # Add titles / figsize
-fig.update_layout(title="Daily Observed Temperature", xaxis_title='Date', yaxis_title='Temperature (°F)', height=1000)
+fig.update_layout(title="Daily Observed Temperature", xaxis_title='Date', yaxis_title='Temperature (°F)', height=1000, template='plotly_dark')
 # Add Slider bar and buttons for year
 fig.update_layout(
     xaxis=dict(
@@ -152,41 +158,22 @@ fig.update_layout(
                      step="year",
                      stepmode="backward"),
                 dict(step="all")
-            ])
+            ]),
+            bgcolor='black',
+            activecolor='darkred',
         ),
         rangeslider=dict(
             visible=True
         ),
         type="date",
-        range=[recent['date'].max() - pd.DateOffset(months=6), recent['date'].max() + pd.DateOffset(days=1)]
-    )
+        range=[recent['date'].max() - pd.DateOffset(months=6), recent['date'].max() + pd.DateOffset(days=1)],
+    ),
+    template='plotly_dark',
 )
 
 # Temp Heatmap
-recent['departure_high'] = recent['max_temp'] - recent['normal_high']
-custom_colors = [
-  '#3b4cc0', '#528ecb', '#7fb8da', '#b5d5e6', 
-  '#e0e0e0',"#e0e0e0", 
-  '#f6bfa6', '#ea7b60', '#c63c36', '#962d20',
-]
 
-fig_heatmap = calplot(
-    recent,
-    x="date",
-    y="departure_high",
-    month_lines=True,
-    month_lines_color='black',
-    month_lines_width=2,
-    colorscale=custom_colors,
-    years_title=True,
-    showscale=True,
-    cmap_min=-20,
-    cmap_max=20,
-    total_height=1200,
-
-)
-
-# App layout
+# Dash layout
 app.layout = dbc.Container([
   dbc.Tabs([
     dcc.Tab(label='Temperature', children=[
@@ -199,14 +186,80 @@ app.layout = dbc.Container([
     ]),
     dcc.Tab(label='Departure from Normal', children = [
       dbc.Row([
-        html.Div("Calendar Heatmap"),
+        html.Div("Daily Heatmap 5 year Departure from Normal"),
+        dcc.Dropdown(
+          options={
+            'max_temp': 'High Temperature',
+            'min_temp':'Low Temperature', 
+            'avg_temp': 'Mean Temperature',
+          },
+          value='max_temp',
+          style={"color": "#000000"},
+          id='daily_heatmap_dropdown',
+        ),
+      html.Div("Select start for 5-year range:"),
+      dcc.Slider(
+          min=int(round(temps_minmax[0]/10)*10),
+          max=temps_minmax[1],
+          step=1,
+          id='heatmap-year-slider',
+          value=2021,
+          marks={str(year): str(year) for year in range(int(round(temps_minmax[0]/10)*10), temps_minmax[1]+1, 5)},
+          tooltip={"placement": "bottom", "always_visible": True},
+          included=False,
+      )      
       ]),
       dbc.Row([
-        dcc.Graph(figure=fig_heatmap, id='calendar_heatmap')
+        dcc.Graph(figure={}, id='calendar_heatmap')
       ]),
     ]),
-  ])
+    ]),
 ], fluid=True)
+
+# Callbacks
+
+# Daily Heatmap
+@callback(
+    Output(component_id='calendar_heatmap', component_property='figure'),
+    Input(component_id='daily_heatmap_dropdown', component_property='value'),
+    Input(component_id='heatmap-year-slider', component_property='value')
+)
+def update_graph(col_chosen, year_start):
+
+  heatmap_df = temps_full[temps_full['year'].between(year_start, year_start+4)].copy()
+  heatmap_df['departure'] = {
+    'max_temp': heatmap_df['max_temp'] - heatmap_df['normal_high'],
+    'min_temp': heatmap_df['min_temp'] - heatmap_df['normal_low'],
+    'avg_temp': heatmap_df['avg_temp'] - heatmap_df['normal_temp'],
+  }[col_chosen]
+
+  custom_colors = [
+    '#3b4cc0', '#528ecb', '#7fb8da', '#b5d5e6', 
+    '#e0e0e0',"#e0e0e0", 
+    '#f6bfa6', '#ea7b60', '#c63c36', '#962d20',
+  ]
+
+  fig_heatmap = calplot(
+      heatmap_df,
+      x="date",
+      y="departure",
+      text=col_chosen,
+      # texttemplate="%{text}°F",
+      month_lines=True,
+      month_lines_color='#333333',
+      month_lines_width=3.5,
+      colorscale=custom_colors,
+      years_title=True,
+      showscale=True,
+      cmap_min=-20,
+      cmap_max=20,
+      total_height=175*5,
+      dark_theme=True,
+      space_between_plots=0.05,
+  )
+  fig_heatmap.update_layout(width=1600)
+
+  return fig_heatmap
 
 
 # Run the app
