6a7f5a57f066bc3fd2973cdcf563708a458e81e5
Jeremy Meyer <jeremymeyer23@hotmail.com>
2025-12-30 19:30:56 -0700

    mood tracker 2025

--- mood_tracker/basic_analysis.py
+++ mood_tracker/basic_analysis.py
@@ -29,6 +29,7 @@ ax.set_xticks([x for x in range(-5,6)])
 plt.title('Rating Count')
 plt.show()
 
+
 # Show rolling 14 vs rolling month avg
 plt.subplots(figsize=(15, 5))
 plt.ylim(bottom=0, top=4)
@@ -39,24 +40,24 @@ plt.title('Moving Average of Mood Rating')
 plt.show()
 
 # Day of week avg
-plt.subplots(figsize=(6.25, 4))
+plt.subplots(figsize=(6.75, 4.75))
 plt.ylim(bottom=0, top=3.75)
 index_ordering = [(x,2023) for x in weekday_order] + [(x,2024) for x in weekday_order] + [(x,2025) for x in weekday_order]
 day_avg = mood_data_raw.groupby(["day_of_week", "year"]).aggregate({'rating' : 'mean'}).reindex(index_ordering)
 day_avg['year'] = [x[1] for x in day_avg.index]
 day_avg['day_of_week'] = [x[0] for x in day_avg.index]
-ax = sns.barplot(x="day_of_week", y='rating', data=day_avg, errorbar=None, hue='year')
+ax = sns.barplot(x="day_of_week", y='rating', data=day_avg, errorbar=None, hue='year', palette='tab10')
 ax.set(ylabel='rating avg', xlabel='weekday', title='Weekday Avg')
 plt.show()
 
 # Month avg
-plt.subplots(figsize=(6.25, 4))
+plt.subplots(figsize=(6.75, 4.75))
 plt.ylim(bottom=0, top=3.5)
 index_ordering = [(x,2023) for x in month_order] + [(x,2024) for x in month_order] + [(x,2025) for x in month_order]
 month_avg = mood_data_raw.groupby(['month', 'year']).aggregate({'rating' : 'mean'}).reindex(index_ordering)
 month_avg['month'] = [x[0][:3] for x in month_avg.index]
 month_avg['year'] = [x[1] for x in month_avg.index]
-ax = sns.barplot(x='month', y='rating', data=month_avg, errorbar=None, hue='year')
+ax = sns.barplot(x='month', y='rating', data=month_avg, errorbar=None, hue='year', palette='tab10')
 ax.set(ylabel='rating avg', xlabel='month', title='Month Avg')
 ax.tick_params(axis='x', which='major', labelsize=8)
 plt.show()
@@ -103,6 +104,21 @@ fig_mood = calplot.calplot(cal_input,
                 cmap="viridis", # Spectral
                 figsize=(16,5.5),
 )
+plt.show()
+
+# Day of week comparison histogram
+recent_data = mood_data_raw.query("date.dt.year == 2025")
+day1 = 'Monday'
+day2 = 'Friday'
+
+plt.figure(figsize=(7, 6))
+sns.histplot(recent_data.query(f"day_of_week=='{day1}'"), x='rating', discrete=True, color='tab:blue', alpha=0.5, label=day1, edgecolor='tab:blue', linewidth=0.75)
+sns.histplot(recent_data.query(f"day_of_week=='{day2}'"), x='rating', discrete=True, color='tab:orange', alpha=0.5, label=day2, edgecolor='tab:orange', linewidth=0.75)
+plt.xticks([x for x in range(-5,6)])
+plt.ylim(bottom=0, top=23)
+plt.title(f'{day1} vs {day2} Rating Count (2025)')
+plt.legend(title=None)
+plt.tight_layout()
 plt.show()
 
 
